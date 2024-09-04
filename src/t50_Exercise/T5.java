cc491bcb475c40d6c587211adc7d2421332d3b81
seunghwanjiji <skinfood8075@naver.com>
2024-09-04 14:49:26 +0900

    20240904_1449 T5.update1

--- src/t50_Exercise/T5.java
+++ src/t50_Exercise/T5.java
@@ -4,5 +4,7 @@ public class T5 {
 	public static void main(String[] args) {
 		System.out.println("안녕");
 		System.out.println("안녕하세요");
+		System.out.println("안녕하세요");
+		System.out.println("안녕하세요");
 	}
 }
